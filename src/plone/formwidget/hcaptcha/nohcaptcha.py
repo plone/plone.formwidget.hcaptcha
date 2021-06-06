f4fab92dbb9197e29182d6ce99b2661baa3c13c9
andreasma <maand@gmx.de>
2021-06-06 20:43:44 +0200

    remove noscript tag, because not supported by non-business hcaptcha account

--- src/plone/formwidget/hcaptcha/nohcaptcha.py
+++ src/plone/formwidget/hcaptcha/nohcaptcha.py
@@ -57,31 +57,6 @@ def displayhtml(
     data-type="%(Type)s"
     data-size="%(Size)s">
 </div>
-<noscript>
-  <div  style="width: 302px; height: 480px;">
-    <div style="width: 302px; height: 422px; position: relative;">
-      <div style="width: 302px; height: 422px; position: relative;">
-        <iframe
-          src="https://www.google.com/hcaptcha/api/fallback?k=%(SiteKey)s&hl=%(LanguageCode)s"
-          frameborder="0" scrolling="no"
-          style="width: 302px; height:422px; border-style: none;">
-        </iframe>
-      </div>
-      <div
-        style="border-style: none; bottom: 12px; left: 25px;
-               margin: 0px; padding: 0px; right: 25px;
-               background: #f9f9f9; border: 1px solid #c1c1c1;
-               border-radius: 3px; height: 60px; width: 300px;">
-            <textarea
-              id="h-captcha-response" name="h-captcha-response"
-              class="h-rcaptcha-response"
-              style="width: 250px; height: 40px; border: 1px solid #c1c1c1;
-                     margin: 10px 25px; padding: 0px; resize: none;"
-              value=""></textarea>
-      </div>
-    </div>
-  </div>
-</noscript>
 """ % {
         "LanguageCode": language,
         "SiteKey": site_key,
